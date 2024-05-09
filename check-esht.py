7c74174ac5558ed7fee964d432849d875ab4a99a
Ebrahim Byagowi <ebrahim@gnu.org>
2024-05-10 00:29:11 +0330

    Minor fix in esht

--- check-esht.py
+++ check-esht.py
@@ -11,7 +11,7 @@ import telegram
 html_page = urlopen(Request('https://eitaa.com/barforooshan')).read()
 
 soup = BeautifulSoup(html_page, 'html.parser')
-latest_post_element = soup.select('.etme_widget_message_photo_wrap')[-1]
+latest_post_element = [x for x in soup.select('.etme_widget_message_bubble') if '#نرخ_قیمت' in x.text][-1].select_one('.etme_widget_message_photo_wrap')
 latest_post = latest_post_element['href']
 img = 'https://eitaa.com/' + cssutils.parseStyle(latest_post_element['style'])['background-image'].replace('url(', '').replace(')', '')
 
