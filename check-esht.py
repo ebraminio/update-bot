3f9c8a8bbdc5f47be76dfb0135252c3515bdc73e
Ebrahim Byagowi <ebrahim@gnu.org>
2024-07-11 14:05:48 +0330

    Use image hash

--- check-esht.py
+++ check-esht.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import asyncio, os
+import asyncio, os, hashlib
 
 from urllib.request import Request, urlopen
 
@@ -14,10 +14,11 @@ soup = BeautifulSoup(html_page, 'html.parser')
 latest_post_element = [x for x in soup.select('.etme_widget_message_bubble') if '#نرخ_قیمت' in x.text][-1].select_one('.etme_widget_message_photo_wrap')
 latest_post = latest_post_element['href']
 img = 'https://eitaa.com/' + cssutils.parseStyle(latest_post_element['style'])['background-image'].replace('url(', '').replace(')', '')
+img_bytes = urlopen(Request(img)).read()
+img_hash = hashlib.sha1(img_bytes).hexdigest()
 
-with open('new-esht', 'r') as f: latest_posted = f.read()
-if latest_posted != latest_post:
-    with open('new-esht', 'w') as f: f.write(latest_post)
+with open('new-esht', 'r') as f: latest_post_hash = f.read()
+if latest_post_hash != img_hash:
+    with open('new-esht', 'w') as f: f.write(img_hash)
     bot = telegram.Bot(os.environ['TELEGRAM_TOKEN'])
-    img_bytes = urlopen(Request(img)).read()
     asyncio.run(bot.send_photo(chat_id='@fruitprices', photo=img_bytes))
