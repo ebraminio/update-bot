fd6bc207eb8df5501218276fd52f37f91fb003fc
Ebrahim <ebrahim@gnu.org>
2026-06-11 09:08:44 +0330

    Improve format

--- check-time.py
+++ check-time.py
@@ -8,6 +8,23 @@ import telegram
 
 from persiancalendar import persian_from_fixed, fixed_from_gregorian
 
+translation_table = str.maketrans("0123456789", "۰۱۲۳۴۵۶۷۸۹")
+def to_persian_num(number): return str(number).translate(translation_table)
+
+persian_months = {
+    1: "فروردین", 2: "اردیبهشت", 3: "خرداد",
+    4: "تیر", 5: "مرداد", 6: "شهریور",
+    7: "مهر", 8: "آبان", 9: "آذر",
+    10: "دی", 11: "بهمن", 12: "اسفند"
+}
+def format_persian_date(year, month, day):
+    p_day = to_persian_num(day)
+    month_name = persian_months.get(month, "")
+    p_year = to_persian_num(year)
+    return f"{p_day} {month_name} {p_year}"
+
+persian_weekdays = ['دوشنبه', 'سه‌شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه', 'شنبه', 'یکشنبه']
+
 with open('new-time', 'r') as f: latest_post = f.read()
 
 async def main():
@@ -16,11 +33,14 @@ async def main():
 
     persian = persian_from_fixed(fixed_from_gregorian((time.year, time.month, time.day)))
     text = '`' + '/'.join(map(str, persian)) + '`\n\n`' + time.date().isoformat() + '`'
+    # print(text)
 
     if text != latest_post:
         with open('new-time', 'w') as f: f.write(text)
-        
+
         bot = telegram.Bot(os.environ['TELEGRAM_TOKEN'])
-        await bot.send_message(chat_id='@ebraminio', parse_mode='markdown', text=text)
+        persian_day = persian_weekdays[time.weekday()]
+        await bot.set_chat_title(chat_id='@ebraminio', title=persian_day + '، ' + format_persian_date(*persian))
+        await bot.send_message(chat_id='@ebraminio', parse_mode='markdown', text=text, disable_notification=True)
 
 asyncio.run(main())
